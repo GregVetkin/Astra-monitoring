52a515406f31f39ef7f64d83145b2c8d95c70bd8
Greg Vetkin <gregoryvetkin@gmail.com>
2023-02-21 16:02:41 +0300

    Function 'cpu_times()' created
    New function in the 'main.py' script
    Return system CPU times.

--- main.py
+++ main.py
@@ -27,7 +27,22 @@ def swap_memory():
     return dict_result
 
 
+def cpu_times(eachcore=True):
+    result = psutil.cpu_times(percpu=eachcore)
+
+    if eachcore:
+        dict_result = {}
+        for core_number, core_info in enumerate(result):
+            dict_result[f'Core {core_number}'] = dict(core_info._asdict())
+
+    else:
+        dict_result = dict(result._asdict())
+
+    return dict_result
+
+
 if __name__ == '__main__':
     #print(vm_detect())
     #print(virtual_memory())
-    print(swap_memory())
+    #print(swap_memory())
+    print(cpu_times())
