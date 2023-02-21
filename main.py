19891ba4387479a6a7f09adafde3df3c77b3c080
Greg Vetkin <gregoryvetkin@gmail.com>
2023-02-21 16:15:08 +0300

    Function 'cpu_percent()' created
    New function in the 'main.py' script
    Returns a floating point number (or a dictionary for all cores) representing the current system-wide CPU usage as a percentage.

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def cpu_times(eachcore=True):
 
     if eachcore:
         dict_result = {}
-        for core_number, core_info in enumerate(result):
-            dict_result[f'Core {core_number}'] = dict(core_info._asdict())
+        for core_number, core_times in enumerate(result):
+            dict_result[f'Core {core_number}'] = dict(core_times._asdict())
 
     else:
         dict_result = dict(result._asdict())
@@ -41,8 +41,22 @@ def cpu_times(eachcore=True):
     return dict_result
 
 
+def cpu_percent(eachcore=True, interval=None):
+    result = psutil.cpu_percent(interval=interval, percpu=eachcore)
+
+    if eachcore:
+        dict_result = {}
+        for core_number, core_percent in enumerate(result):
+            dict_result[f'Core {core_number}'] = core_percent
+        return dict_result
+
+    else:
+        return result
+
+
 if __name__ == '__main__':
     #print(vm_detect())
     #print(virtual_memory())
     #print(swap_memory())
-    print(cpu_times())
+    #print(cpu_times())
+    print(cpu_percent())
