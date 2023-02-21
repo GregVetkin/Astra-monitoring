548020f5ad51c051535d8d31749278615f80afc7
Greg Vetkin <gregoryvetkin@gmail.com>
2023-02-21 16:25:50 +0300

    Function 'cpu_count()' created
    New function in the 'main.py' script.
    Returns a dictionary with the keys "physical" and "logical" containing the value of the number of cores.

--- main.py
+++ main.py
@@ -54,9 +54,18 @@ def cpu_percent(eachcore=True, interval=None):
         return result
 
 
+def cpu_count():
+    phys_cores = psutil.cpu_count(logical=False)
+    logic_cores = psutil.cpu_count(logical=True)
+    dict_result = {'physical': phys_cores, 'logical': logic_cores}
+
+    return dict_result
+
+
 if __name__ == '__main__':
     #print(vm_detect())
     #print(virtual_memory())
     #print(swap_memory())
     #print(cpu_times())
-    print(cpu_percent())
+    #print(cpu_percent())
+    print(cpu_count())
