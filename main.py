0e3b9619afcda647c45edd39aaea5329e7d33780
Greg Vetkin <gregoryvetkin@gmail.com>
2023-02-22 15:10:13 +0300

    Creation of the 'disks_info' function
    An unfinished function that collects information about partitions, io and usage

--- main.py
+++ main.py
@@ -84,6 +84,40 @@ def cpu_freq(eachcore=True):
     return dict_result
 
 
+def disks_info():
+    dict_result = {}
+    disks = psutil.disk_partitions(all=False)
+    disks_io_counters = psutil.disk_io_counters(perdisk=True, nowrap=True)
+
+    for disk_io in disks_io_counters:
+        if 'sda' in disk_io:
+            print(disks_io_counters[disk_io])
+
+    for disk_number, disk in enumerate(disks):
+        dict_result[f'Disk {disk_number}'] = {}
+        dict_result[f'Disk {disk_number}']['device'] = disk.device
+        dict_result[f'Disk {disk_number}']['mountpoint'] = disk.mountpoint
+        dict_result[f'Disk {disk_number}']['fstype'] = disk.fstype
+        dict_result[f'Disk {disk_number}']['opts'] = disk.opts
+        dict_result[f'Disk {disk_number}']['maxfile'] = disk.maxfile
+        dict_result[f'Disk {disk_number}']['maxpath'] = disk.maxpath
+
+        if 'rw' in disk.opts:
+            disk_usage = psutil.disk_usage(disk.mountpoint)
+            dict_result[f'Disk {disk_number}']['total'] = disk_usage.total
+            dict_result[f'Disk {disk_number}']['used'] = disk_usage.used
+            dict_result[f'Disk {disk_number}']['free'] = disk_usage.free
+            dict_result[f'Disk {disk_number}']['percent'] = disk_usage.percent
+
+        else:
+            dict_result[f'Disk {disk_number}']['total'] = None
+            dict_result[f'Disk {disk_number}']['used'] = None
+            dict_result[f'Disk {disk_number}']['free'] = None
+            dict_result[f'Disk {disk_number}']['percent'] = None
+
+    return dict_result
+
+
 if __name__ == '__main__':
     #print(vm_detect())
     #print(virtual_memory())
@@ -92,4 +126,5 @@ if __name__ == '__main__':
     #print(cpu_percent())
     #print(cpu_count())
     #print(cpu_stats())
-    print(cpu_freq())
+    #print(cpu_freq())
+    print(disks_info())
